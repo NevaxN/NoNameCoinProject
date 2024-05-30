a56a9b32178a14bee6588093097a5ddc924af164
nevaxN <arthurmayersantos@gmail.com>
2024-05-30 18:13:13 -0300

    rotas a serem utilizadas no projeto adicionadas, ainda sendo bem simples

--- services/app.py
+++ services/app.py
@@ -2,5 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def conexao_banco():
+    return "Funcionando"
+
+@app.route('/trans')
+def transacoes():
+    return "Funcionando"
+
+@app.route('/seletor')
+def seletor():
+    return "Funcionando"
+
+@app.route('/validador')
+def validador():
+    return "Funcionando"
+
+@app.route('/hora')
+def hora():
+    return "Funcionando"
+
+@app.errorhandler(400)
+def bad_request(error):
+    return "Erro ao carregar página", 400
+
 if __name__ == "__main__":
     app.run(debug=True)
