136accaabcf0789ccea6504d0fbf1199341a48f8
nevaxN <arthurmayersantos@gmail.com>
2024-06-01 19:57:58 -0300

    arquivos e pastas deletadas

--- model/transacao.py
+++
@@ -1,8 +0,0 @@
-class Transacao:
-    def __init__(self):
-        self.sender = ''
-        self.receiver = ''
-        self.amount = 0.0
-        self.taxa = 0.0
-        self.timestamp = '(função para timestamp)'
-        self.chave_validador = ''
