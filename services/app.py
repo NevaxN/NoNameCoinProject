350cb4df62570f60ce097a276c92946f7bb283bd
nevaxN <arthurmayersantos@gmail.com>
2024-07-02 09:43:12 -0300

    Adicionando novas tabelas e parametros no Gerenciador, novas funções para melhor funcionamento das interações de Classes

--- services/app.py
+++ services/app.py
@@ -41,10 +41,12 @@ class Seletor(db.Model):
     id: int
     nome: str
     ip: str
+    saldo: int
 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(20), unique=False, nullable=False)
     ip = db.Column(db.String(15), unique=False, nullable=False)
+    saldo = db.Column(db.Integer, unique=False, nullable=False)
 
 
 @dataclass
@@ -75,6 +77,7 @@ class Validador(db.Model):
     selecoes_consecutivas: int
     transacoes_coerentes: int
     horario_ultima_transacao: datetime
+    expulsoes: int
 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(20), unique=False, nullable=False)
@@ -84,7 +87,20 @@ class Validador(db.Model):
     hold = db.Column(db.Integer, nullable=False, default=0)
     selecoes_consecutivas = db.Column(db.Integer, nullable=False, default=0)
     transacoes_coerentes = db.Column(db.Integer, nullable=False, default=0)
-    horario_ultima_transacao = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.now())
+    horario_ultima_transacao = db.Column(db.DateTime, unique=False, nullable=False)
+    expulsoes = db.Column(db.Integer, default=0)
+
+
+class EleicaoLog(db.Model):
+    id: int
+    timestamp: datetime
+    acao: str
+    detalhes: str
+
+    id = db.Column(db.Integer, primary_key=True),
+    timestamp = db.Column(db.DateTime, unique=False, default=datetime.utcnow),
+    acao = db.Column(db.String(100), unique=True, nullable=False),
+    detalhes = db.Column(db.String(250), unique=True, nullable=False)
 
 
 with app.app_context():
@@ -164,10 +180,10 @@ def ListarSeletor():
         return jsonify({"Seletores:": seletores})
 
 
-@app.route('/seletor/<string:nome>/<string:ip>', methods=['POST'])
-def InserirSeletor(nome, ip):
-    if request.method == 'POST' and nome != '' and ip != '':
-        objeto = Seletor(nome=nome, ip=ip)
+@app.route('/seletor/<string:nome>/<string:ip>/<int:saldo>', methods=['POST'])
+def InserirSeletor(nome, ip, saldo):
+    if request.method == 'POST' and nome != '' and ip != '' and saldo > 0:
+        objeto = Seletor(nome=nome, ip=ip, saldo=saldo)
         db.session.add(objeto)
         db.session.commit()
         return jsonify({"Seletor:": objeto})
@@ -186,8 +202,8 @@ def UmSeletor(id):
         return jsonify({'message': 'Method Not Allowed'}), 405
 
 
-@app.route('/seletor/<int:id>/<string:nome>/<string:ip>', methods=["POST"])
-def EditarSeletor(id, nome, ip):
+@app.route('/seletor/<int:id>/<string:nome>/<string:ip>/<int:saldo>', methods=["POST"])
+def EditarSeletor(id, nome, ip, saldo):
     if request.method == 'POST':
         try:
             seletor = Seletor.query.filter_by(id=id).first()
@@ -196,6 +212,7 @@ def EditarSeletor(id, nome, ip):
 
             seletor.nome = nome
             seletor.ip = ip
+            seletor.saldo = saldo
             db.session.commit()
             return jsonify({"Seletor:": seletor})
         except Exception as e:
@@ -222,8 +239,8 @@ def ApagarSeletor(id):
 
 @app.route('/hora', methods=['GET'])
 def horario():
-    current_time = datetime.now()
-    return jsonify({'horario': current_time})
+    current_time = datetime.utcnow()
+    return jsonify({'horario': current_time.isoformat()})
 
 
 @app.route('/transacoes', methods=['GET'])
@@ -273,9 +290,29 @@ def CriaTransacao(rem, reb, valor):
         if len(validadores) < 3:
             return jsonify({"message": "Não há validadores suficientes disponíveis"}), 503
 
+        validadores_selecionados = []
+        for validador in validadores:
+            chance_selecao = 1.0  # 100% de chance inicialmente
+
+            if validador.flags == 1:
+                chance_selecao *= 0.5  # Reduz em 50% a chance de seleção
+            elif validador.flags == 2:
+                chance_selecao *= 0.25  # Reduz em 75% a chance de seleção
+
+            # Gerar um número aleatório entre 0 e 1 para decidir se seleciona o validador
+            if random.random() < chance_selecao:
+                validadores_selecionados.append(validador)
+
         # Limitar a seleção a 20% do total de validadores
         max_validadores = max(3, int(len(validadores) * 0.2))
-        validadores_selecionados = random.choices(validadores, k=min(3, max_validadores))
+        validadores_ordenados = sorted(validadores, key=lambda v: (v.flags, -v.saldo))
+
+        # Selecionar os top `max_validadores` validadores
+        validadores_selecionados = validadores_ordenados[:max_validadores]
+
+        # Exibir os validadores selecionados
+        for validador in validadores_selecionados:
+            print(f"Validador ID: {validador.id}, Flags: {validador.flags}, Saldo: {validador.saldo}")
 
         # Chamar a função de validação com os IDs do remetente e recebedor
         status_final = enviar_validacao(transacao, validadores_selecionados, remetente, recebedor)
@@ -288,14 +325,14 @@ def CriaTransacao(rem, reb, valor):
         return jsonify({"message": "Erro ao criar transação"}), 500
 
 
-@app.route('/transacoes/<int:id>/<int:status>', methods=["POST"])
-def EditaTransacao(id, status):
+@app.route('/transacoes/<int:id>', methods=["POST"])
+def ValidarTransacaoUnica(id):
     try:
         transacao = Transacao.query.filter_by(id=id).first()
+
         if not transacao:
             return jsonify({"message": "Transação não encontrada"}), 404
 
-        transacao.status = status
         db.session.commit()
 
         remetente = Cliente.query.get(transacao.remetente)
@@ -304,12 +341,52 @@ def EditaTransacao(id, status):
         if not remetente or not recebedor:
             return jsonify({"message": "Remetente ou recebedor não encontrado"}), 404
 
-        # Obter os validadores
-        seletores = Seletor.query.all()
-        validadores = random.sample(seletores, min(3, len(seletores)))
+        # Obter os validadores, ordenando por flags e saldo
+        validadores = Validador.query.order_by(Validador.flags.asc(), Validador.saldo.desc()).all()
+
+        # Esperar até que pelo menos três validadores estejam disponíveis, ou até um minuto
+        tempo_limite = datetime.now() + timedelta(minutes=1)
+        while len(validadores) < 3 and datetime.now() < tempo_limite:
+            app.logger.info(
+                f"Esperando validadores. Tempo restante: {(tempo_limite - datetime.now()).seconds} segundos")
+            sleep(1)
+            validadores = Validador.query.order_by(Validador.flags.asc(), Validador.saldo.desc()).all()
+
+        if len(validadores) < 3:
+            return jsonify({"message": "Não há validadores suficientes disponíveis"}), 503
+
+        validadores_selecionados = []
+        for validador in validadores:
+            chance_selecao = 1.0  # 100% de chance inicialmente
+
+            if validador.flags == 1:
+                chance_selecao *= 0.5  # Reduz em 50% a chance de seleção
+            elif validador.flags == 2:
+                chance_selecao *= 0.25  # Reduz em 75% a chance de seleção
+
+            # Gerar um número aleatório entre 0 e 1 para decidir se seleciona o validador
+            if random.random() < chance_selecao:
+                validadores_selecionados.append(validador)
+
+        registrar_log('Início da eleição', 'Iniciando o processo de eleição dos validadores.')
+
+        max_validadores = max(3, int(len(validadores) * 0.2))
+        validadores_ordenados = sorted(validadores, key=lambda v: (v.flags, -v.saldo))
+
+        # Selecionar os top `max_validadores` validadores
+        validadores_selecionados = validadores_ordenados[:max_validadores]
+
+        registrar_log('Seleção dos validadores',
+                      f'Validadores selecionados: {[v.id for v in validadores_selecionados]}')
+
+        # Exibir os validadores selecionados
+        for validador in validadores_selecionados:
+            print(f"Validador ID: {validador.id}, Flags: {validador.flags}, Saldo: {validador.saldo}")
+            registrar_log('Detalhes do validador selecionado',
+                          f'ID: {validador.id}, Flags: {validador.flags}, Saldo: {validador.saldo}')
 
         # Chamar a função de validação com os IDs do remetente e recebedor
-        status_final = enviar_validacao(transacao, validadores, remetente, recebedor)
+        status_final = enviar_validacao(transacao, validadores_selecionados, remetente, recebedor)
 
         return jsonify({"id": transacao.id, "status": status_final})
 
@@ -321,13 +398,15 @@ def EditaTransacao(id, status):
 def enviar_validacao(transacao, validadores, rem, rec):
     try:
         respostas = []
-        recompensa = 10  # Define a recompensa para cada validador
         manter_chave_cheia()
+        status_validadores = []
+        seletor_id = 1
+        seletor = Seletor.query.get(seletor_id)
 
         for validador in validadores:
             status = 0
             # Regras de validação
-            if transacao.valor >= rem.qtdMoeda:
+            if transacao.valor > rem.qtdMoeda:
                 print("Não Aprovada Valor")
                 status = STATUS_NAO_APROVADA
             elif validador.chave_unica != CHAVES[str(validador.id)]:
@@ -336,33 +415,74 @@ def enviar_validacao(transacao, validadores, rem, rec):
             elif validador.saldo < 100:  # Exemplo de saldo mínimo
                 print("Não Aprovada Saldo")
                 status = STATUS_NAO_APROVADA
-            elif validador.horario_ultima_transacao > transacao.horario > datetime.now():  # Exemplo de horário de operação
+            elif validador.horario_ultima_transacao > transacao.horario > datetime.now():  # Exemplo de horário de
+                # operação
                 print("Não Aprovada Hora")
+                status = STATUS_NAO_APROVADA
+            elif validador.transacoes_coerentes > 100:
                 status = STATUS_NAO_APROVADA
             else:
                 status = STATUS_TRANSACAO_CONCLUIDA
 
+            status_validadores.append(status)
+
             respostas.append({"validador_id": validador.id, "status": status})
 
-            if status == STATUS_NAO_APROVADA:
-                validador.flags += 1
-                if validador.flags > 5:
-                    db.session.delete(validador)
-                    db.session.commit()
-                else:
-                    db.session.commit()
-            else:
-                validador.saldo += recompensa
-                db.session.commit()
+            validador.horario_ultima_transacao = datetime.now()
 
-            # validador.escolhas_consecutivas += 1
-            # if validador.escolhas_consecutivas > 5:
-            #     validador.escolhas_consecutivas = 0
-            #     validador.hold_ate = datetime.now() + timedelta(minutes=5)
-            #     db.session.commit()
+            validador.selecoes_consecutivas += 1
+            if validador.selecoes_consecutivas > 5:
+                validador.selecoes_consecutivas = 0
+                validador.hold_ate = datetime.now() + timedelta(minutes=5)
+                db.session.commit()
 
         aprovacoes = sum(1 for r in respostas if r["status"] == STATUS_TRANSACAO_CONCLUIDA)
         status_final = STATUS_TRANSACAO_CONCLUIDA if aprovacoes >= 2 else STATUS_NAO_APROVADA
+
+        if aprovacoes >= 2:
+            recompensa_total = transacao.valor * 0.02
+            recompensa_seletor = transacao.valor * 0.015
+            recompensa_validador_travada = transacao.valor * 0.005
+            recompensa_validadores = recompensa_total - recompensa_seletor - recompensa_validador_travada
+            recompensa_por_validador = recompensa_validadores / len(validadores)
+
+            # Atualiza o saldo do seletor
+            seletor.saldo += recompensa_seletor
+            db.session.commit()
+
+            for i in range(len(status_validadores)):
+                if status_validadores[i] != status_final:
+                    validadores[i].flags += 1
+                    if validadores[i].flags >= 2:
+                        db.session.delete(validadores[i])
+                        validadores[i].expulsoes += 1
+                        db.session.commit()
+                        if validadores[i].expulsoes <= 2:
+                            saldo_travado = validadores[i].saldo
+                            db.session.delete(validadores[i])
+                            db.session.commit()
+                            novo_validador = Validador(
+                                nome='novo_validador',
+                                saldo=saldo_travado * 2,
+                                chave_unica=CHAVES[str(validadores[i].id)],
+                                horario_ultima_transacao=datetime.now()
+                            )
+                            db.session.add(novo_validador)
+                            db.session.commit()
+                        else:
+                            db.session.delete(validadores[i])
+                            db.session.commit()
+                    else:
+                        db.session.commit()
+                else:
+                    validadores[i].saldo += recompensa_por_validador
+                    validadores[i].saldo += recompensa_validador_travada
+                    validadores[i].transacoes_coerentes += 1
+
+                    if validadores[i].transacoes_coerentes % 10000 == 0:
+                        validadores[i].flags = max(0, validadores[i].flags - 1)
+
+                    db.session.commit()
 
         transacao.status = status_final
         db.session.commit()
@@ -388,7 +508,9 @@ def inserir_validador(nome, saldo):
         try:
             validador = Validador()
             chave_unica = cadastrar_chave_unica_validador(validador.id)
-            validador = Validador(nome=nome, saldo=saldo, flags=0, hold=0, chave_unica=chave_unica)
+            validador = Validador(nome=nome, saldo=saldo - 50, flags=0, hold=0, chave_unica=chave_unica,
+                                  transacoes_coerentes=0, selecoes_consecutivas=0,
+                                  horario_ultima_transacao=datetime.now())
             db.session.add(validador)
             db.session.commit()
             return jsonify({"id": validador.id})
@@ -427,6 +549,24 @@ def manter_chave_cheia():
 
     for validador in validadores:
         CHAVES[str(validador.id)] = validador.chave_unica
+
+
+def registrar_log(acao, detalhes=None):
+    log = EleicaoLog(acao=acao, detalhes=detalhes)
+    db.session.add(log)
+    db.session.commit()
+
+
+def sincronizar_tempo():
+    import requests
+    response = requests.get('http://localhost:5000/hora')
+    if response.status_code == 200:
+        server_time_str = response.json()['horario']
+        server_time = datetime.fromisoformat(server_time_str)
+        # Aqui você pode ajustar o tempo do validador conforme necessário
+        print(f"Tempo do servidor sincronizado: {server_time}")
+    else:
+        print("Falha ao sincronizar o tempo")
 
 
 if __name__ == "__main__":
