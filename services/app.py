790408142bdea76b123d68aef143744fc542e04c
andreykusman <144070661+andreykusman@users.noreply.github.com>
2024-07-02 16:13:08 -0300

    comentarios

--- services/app.py
+++ services/app.py
@@ -110,50 +110,60 @@ with app.app_context():
 
 @app.route("/")
 def index():
+    # Rota principal da API. Retorna uma mensagem de boas-vindas.
     return jsonify({'message': 'API sem interface do banco!'})
 
 
 @app.route('/cliente', methods=['GET'])
 def ListarCliente():
-    clientes = Cliente.query.all()
-    return jsonify({"Clientes": clientes})
+    clientes = Cliente.query.all() # Consulta todos os clientes no banco de dados.
+    return jsonify({"Clientes": clientes}) # Retorna a lista de clientes em formato JSON.
 
 
 @app.route('/cliente/<string:nome>/<string:senha>/<int:qtdMoeda>', methods=['POST'])
+ # Rota para inserir um novo cliente. Recebe nome, senha e quantidade de moeda como parâmetros.
 def InserirCliente(nome, senha, qtdMoeda):
     if request.method == 'POST' and nome != '' and senha != '' and qtdMoeda != '':
-        objeto = Cliente(nome=nome, senha=senha, qtdMoeda=qtdMoeda)
-        db.session.add(objeto)
-        db.session.commit()
-        return jsonify({"Cliente": objeto})
+        # Verifica se o método é POST e os parâmetros não estão vazios.
+        objeto = Cliente(nome=nome, senha=senha, qtdMoeda=qtdMoeda) # Cria um novo cliente.
+        db.session.add(objeto) # Adiciona o cliente à sessão do banco de dados
+        db.session.commit() # Salva as alterações no banco de dados.
+        return jsonify({"Cliente": objeto}) # Retorna o cliente inserido em formato JSON.
     else:
+        # Caso contrário, retorna um erro indicando que o método não é permitido.
         return jsonify({'message': 'Method Not Allowed'}), 405
 
 
 @app.route('/cliente/<int:id>', methods=['GET'])
 def UmCliente(id):
+        # Rota para obter um cliente específico pelo ID.
     if request.method == 'GET':
-        objeto = Cliente.query.get(id)
+        # Verifica se o método é GET.
+        objeto = Cliente.query.get(id)# Consulta o cliente com o ID fornecido.
+        # Se o cliente não for encontrado, retorna um erro 404.
         if not objeto:
             return jsonify({"message": "Cliente não encontrado"}), 404
-        return jsonify({"Cliente": objeto})
+        return jsonify({"Cliente": objeto})# Retorna o cliente encontrado em formato JSON.
     else:
+        # Caso contrário, retorna um erro indicando que o método não é permitido.
         return jsonify({'message': 'Method Not Allowed'}), 405
 
 
 @app.route('/cliente/<int:id>/<int:qtdMoedas>', methods=["POST"])
 def EditarCliente(id, qtdMoedas):
+    # Rota para editar a quantidade de moedas de um cliente específico.
     if request.method == 'POST':
+        # Verifica se o método é POST.
         try:
             cliente = Cliente.query.filter_by(id=id).first()
-
+            # Consulta o cliente com o ID fornecido.
             if cliente is None:
                 return jsonify({"message": "Cliente não encontrado"}), 404
-
-            cliente.qtdMoedas = qtdMoedas
-            db.session.commit()
+            # Se o cliente não for encontrado, retorna um erro 404.
+            cliente.qtdMoedas = qtdMoedas # Atualiza a quantidade de moedas do cliente.
+            db.session.commit() # Salva as alterações no banco de dados.
             return jsonify({"message": "Alteração feita com sucesso"}), 200
-        except Exception as e:
+        except Exception as e: # Se ocorrer uma exceção, registra o erro e retorna uma mensagem de erro 500.
             app.logger.error(f"Erro ao atualizar o cliente: {e}")
             return jsonify({"message": "Atualização não realizada"}), 500
 
@@ -177,16 +187,17 @@ def ApagarCliente(id):
 @app.route('/seletor', methods=['GET'])
 def ListarSeletor():
     if request.method == 'GET':
-        seletores = Seletor.query.all()
+        seletores = Seletor.query.all() # Consulta todos os seletores no banco de dados.
         return jsonify({"Seletores:": seletores})
 
 
 @app.route('/seletor/<string:nome>/<string:ip>/<int:saldo>', methods=['POST'])
 def InserirSeletor(nome, ip, saldo):
     if request.method == 'POST' and nome != '' and ip != '' and saldo > 0:
-        objeto = Seletor(nome=nome, ip=ip, saldo=saldo)
-        db.session.add(objeto)
-        db.session.commit()
+        # Verifica se o método é POST e os parâmetros são válidos.
+        objeto = Seletor(nome=nome, ip=ip, saldo=saldo) # Cria um novo seletor.
+        db.session.add(objeto)  # Adiciona o seletor à sessão do banco de dados.
+        db.session.commit() 
         return jsonify({"Seletor:": objeto})
     else:
         return jsonify({'message': 'Method Not Allowed'}), 405
@@ -195,7 +206,7 @@ def InserirSeletor(nome, ip, saldo):
 @app.route('/seletor/<int:id>', methods=['GET'])
 def UmSeletor(id):
     if request.method == 'GET':
-        seletor = Seletor.query.get(id)
+        seletor = Seletor.query.get(id) # Consulta o seletor com o ID fornecido.
         if not seletor:
             return jsonify({"message": "Seletor não encontrado"}), 404
         return jsonify({"Seletor:": seletor})
@@ -207,15 +218,15 @@ def UmSeletor(id):
 def EditarSeletor(id, nome, ip, saldo):
     if request.method == 'POST':
         try:
-            seletor = Seletor.query.filter_by(id=id).first()
+            seletor = Seletor.query.filter_by(id=id).first()  # Consulta o seletor com o ID fornecido.
             if not seletor:
                 return jsonify({"message": "Seletor não encontrado"}), 404
 
-            seletor.nome = nome
-            seletor.ip = ip
-            seletor.saldo = saldo
-            db.session.commit()
-            return jsonify({"Seletor:": seletor})
+            seletor.nome = nome # Atualiza o nome do seletor.
+            seletor.ip = ip  # Atualiza o IP do seletor.
+            seletor.saldo = saldo # Atualiza o saldo do seletor.
+            db.session.commit() # Salva as alterações no banco de dados.
+            return jsonify({"Seletor:": seletor}) # Retorna o seletor atualizado em formato JSON.
         except Exception as e:
             data = {
                 "message": f"Atualização não realizada; {e}",
@@ -240,23 +251,25 @@ def ApagarSeletor(id):
 
 @app.route('/hora', methods=['GET'])
 def horario():
-    current_time = datetime.utcnow()
+    current_time = datetime.utcnow()  # Obtém o horário atual em UTC.
     return jsonify({'horario': current_time.isoformat()})
 
 
 @app.route('/transacoes', methods=['GET'])
 def ListarTransacoes():
     if request.method == 'GET':
-        transacoes = Transacao.query.all()
+        transacoes = Transacao.query.all() # Consulta todas as transações no banco de dados.
         return jsonify({"Transações:": transacoes})
 
 
 @app.route('/transacoes/<int:rem>/<int:reb>/<int:valor>', methods=['POST'])
 def CriaTransacao(rem, reb, valor):
+    # Rota para criar uma nova transação. Verifica a validade da transação e seleciona validadores.
     try:
-        remetente = Cliente.query.get(rem)
-        recebedor = Cliente.query.get(reb)
+        remetente = Cliente.query.get(rem) # Consulta o cliente remetente.
+        recebedor = Cliente.query.get(reb) # Consulta o cliente recebedor.
 
+        # Se o remetente não for encontrado, retorna um erro 404.
         if not remetente:
             return jsonify({"message": "Remetente não encontrado"}), 404
         if not recebedor:
@@ -265,17 +278,17 @@ def CriaTransacao(rem, reb, valor):
         if remetente.qtdMoeda < valor:
             return jsonify({"message": "Saldo insuficiente"}), 400
 
-        um_minuto_atras = datetime.now() - timedelta(minutes=1)
+        um_minuto_atras = datetime.now() - timedelta(minutes=1) # Define o limite de um minuto.
         transacoes_recentes = Transacao.query.filter(Transacao.remetente == rem,
-                                                     Transacao.horario >= um_minuto_atras).count()
+                                                     Transacao.horario >= um_minuto_atras).count()  # Conta transações recentes.
 
         if transacoes_recentes >= 100:
             return jsonify({"message": "Limite de transações por minuto excedido"}), 429
 
         transacao = Transacao(remetente=rem, recebedor=reb, valor=valor, status=STATUS_NAO_EXECUTADA,
-                              horario=datetime.now())
-        db.session.add(transacao)
-        db.session.commit()
+                              horario=datetime.now())  # Cria uma nova transação com status não executada.
+        db.session.add(transacao) # Adiciona a transação à sessão do banco de dados.
+        db.session.commit()  # Salva as alterações no banco de dados.
 
         # Obter os validadores, ordenando por flags e saldo
         validadores = Validador.query.order_by(Validador.flags.asc(), Validador.saldo.desc()).all()
@@ -406,18 +419,20 @@ def ValidarTransacaoUnica(id):
 
 def enviar_validacao(transacao, validadores, rem, rec):
     try:
-        respostas = []
-        manter_chave_cheia()
-        status_validadores = []
-        seletor_id = 1
-        seletor = Seletor.query.get(seletor_id)
+        respostas = [] # Lista para armazenar as respostas de cada validador.
+        manter_chave_cheia() # Função para garantir que a chave está cheia.
+        status_validadores = [] # Lista para armazenar o status de cada validador.
+        seletor_id = 1 # ID do seletor (presumivelmente fixo para este exemplo).
+        seletor = Seletor.query.get(seletor_id) # Consulta o seletor com o ID fornecido.
 
+        # Itera sobre cada validador para verificar as regras de validação.
         for validador in validadores:
             status = 0
-            # Regras de validação
+            # Verifica se a transação tem valor maior que o saldo do remetente.
             if transacao.valor > rem.qtdMoeda:
                 print("Não Aprovada Valor")
                 status = STATUS_NAO_APROVADA
+            # Verifica se a chave única do validador é inválida.
             elif validador.chave_unica != CHAVES[str(validador.id)]:
                 print("Não Aprovada Chave Unica")
                 status = STATUS_NAO_APROVADA
@@ -428,6 +443,7 @@ def enviar_validacao(transacao, validadores, rem, rec):
                 # operação
                 print("Não Aprovada Hora")
                 status = STATUS_NAO_APROVADA
+            # Verifica se o número de transações coerentes do validador é muito alto.
             elif validador.transacoes_coerentes > 100:
                 status = STATUS_NAO_APROVADA
             else:
@@ -437,18 +453,25 @@ def enviar_validacao(transacao, validadores, rem, rec):
 
             respostas.append({"validador_id": validador.id, "status": status})
 
+            # Atualiza o horário da última transação do validador.
             validador.horario_ultima_transacao = datetime.now()
+
+            # Atualiza o número de seleções consecutivas do validador.
 
             validador.selecoes_consecutivas += 1
             if validador.selecoes_consecutivas > 5:
+                # Se o número de seleções consecutivas for maior que 5, coloca o validador em HOLD.
                 validador.hold += 1  # Coloca o validador em HOLD pelas próximas 5 transações
                 validador.selecoes_consecutivas = 0
                 validador.hold_ate = datetime.now() + timedelta(seconds=10)
+                # Se o HOLD tiver expirado, reseta o estado do validador.
                 if validador.hold_ate == datetime.now():
                     validador.hold = 0
                 db.session.commit()
 
+        # Conta o número de validações bem-sucedidas.
         aprovacoes = sum(1 for r in respostas if r["status"] == STATUS_TRANSACAO_CONCLUIDA)
+        # Define o status final da transação baseado no número de aprovações.       
         status_final = STATUS_TRANSACAO_CONCLUIDA if aprovacoes >= 2 else STATUS_NAO_APROVADA
 
         if aprovacoes >= 2:
@@ -464,8 +487,11 @@ def enviar_validacao(transacao, validadores, rem, rec):
 
             for i in range(len(status_validadores)):
                 if status_validadores[i] != status_final:
+                    # Se o status do validador não corresponder ao status final, aumenta o número de flags do validador.
+
                     validadores[i].flags += 1
                     if validadores[i].flags >= 2:
+                        # Se o número de flags exceder o limite, exclui o validador e cria um novo.
                         db.session.delete(validadores[i])
                         validadores[i].expulsoes += 1
                         db.session.commit()
@@ -487,19 +513,22 @@ def enviar_validacao(transacao, validadores, rem, rec):
                     else:
                         db.session.commit()
                 else:
+                    # Se o status do validador corresponder ao status final, atualiza seu saldo e transações coerentes.
                     validadores[i].saldo += recompensa_por_validador
                     validadores[i].saldo += recompensa_validador_travada
                     validadores[i].transacoes_coerentes += 1
 
+                    # Se o número de transações coerentes atingir um múltiplo de 10.000, reduz o número de flags.
                     if validadores[i].transacoes_coerentes % 10000 == 0:
                         validadores[i].flags = max(0, validadores[i].flags - 1)
 
                     db.session.commit()
 
+        # Atualiza o status da transação no banco de dados.
         transacao.status = status_final
         db.session.commit()
 
-        sincronizar_tempo()
+        sincronizar_tempo() # Função para sincronizar o tempo.
 
         return status_final
     except Exception as e:
